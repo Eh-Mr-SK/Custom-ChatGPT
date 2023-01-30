72016117fda4784467905a8a294d20fdb4123ed7
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-01-30 21:57:00 +0600

    added frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [completion, setCompletion] = useState('');
+  const [prompt, setPrompt] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3001/completion', {
+        prompt: prompt
+      });
+      setCompletion(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter prompt" onChange={(e) => setPrompt(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      <p>Completion: {completion}</p>
     </div>
   );
-}
+};
 
 export default App;
