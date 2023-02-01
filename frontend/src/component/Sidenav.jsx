019de3c58257d88c616fe87295167b4083074b76
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-02-01 19:06:37 +0600

    added sidebar

--- frontend/src/component/Sidenav.jsx
+++ frontend/src/component/Sidenav.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import '../App.css'
 
 function Sidenav() {
     return (
         <div>
-            
+            <body>
+
+                <div class="sidenav">
+                    <a href="#about">About</a>
+                    <a href="#services">Services</a>
+                    <a href="#clients">Clients</a>
+                    <a href="#contact">Contact</a>
+                </div>
+
+                <div class="main">
+                    <h2>Sidebar</h2>
+                  
+                </div>
+
+            </body>
         </div>
     );
 }
