46fb5659116adecebe1bcb77efccd8bb5aa546f3
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-02-01 18:59:16 +0600

    div turn into left side

--- frontend/src/component/Chat.jsx
+++ frontend/src/component/Chat.jsx
@@ -3,107 +3,115 @@ import axios from 'axios';
 import { useSpeechRecognition } from 'react-speech-kit';
 
 const Chat = () => {
-  const [completions, setCompletions] = useState([]);
-  const [prompt, setPrompt] = useState('');
-  const { listen, stop } = useSpeechRecognition({
-    onResult: (result) => {
-      setPrompt(result);
-    },
-  });
+    const [completions, setCompletions] = useState([]);
+    const [prompt, setPrompt] = useState('');
+    const { listen, stop } = useSpeechRecognition({
+        onResult: (result) => {
+            setPrompt(result);
+        },
+    });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:5000/completion', {
-        prompt: prompt
-      });
-      setCompletions([...completions, { prompt, completion: response.data }]);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:5000/completion', {
+                prompt: prompt
+            });
+            setCompletions([...completions, { prompt, completion: response.data }]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  return (
-    <div style={{ display: "", justifyContent: "center", flexDirection: "column" }}>
-      <div style={{ width: "500px", padding: "20px" }}>
 
-        <div style={{ display: "", justifyContent: "center", flexDirection: "column", marginTop: "20px" }}>
-          {completions.map((msg, index) => (
-            <div key={index} style={{ display: "", justifyContent: "space-between", marginBottom: "10px" }}>
-              <div style={{
-                backgroundColor: "lightgray",
-                padding: "10px",
-                borderRadius: "10px",
-                width: "90%"
-              }}>
-                <p>You: {msg.prompt}</p>
-              </div>
-              &nbsp;
-              <div style={{
-                backgroundColor: "lightblue",
-                padding: "10px",
-                borderRadius: "10px",
-                width: "90%"
-              }}>
-                <p>Completion: {msg.completion}</p>
-              </div>
-              &nbsp;
+
+
+    return (
+        <div style={{ float: "right", display: "", justifyContent: "center", flexDirection: "column" }}>
+            <div style={{ width: "500px", padding: "20px" }}>
+
+                <div style={{ display: "", justifyContent: "center", flexDirection: "column", marginTop: "20px" }}>
+                    {completions.map((msg, index) => (
+                        <div key={index} style={{ display: "", justifyContent: "space-between", marginBottom: "10px" }}>
+                            <div style={{
+                                backgroundColor: "lightgray",
+                                padding: "10px",
+                                borderRadius: "10px",
+                                width: "90%"
+                            }}>
+                                <p>You: {msg.prompt}</p>
+                            </div>
+                            &nbsp;
+                            <div style={{
+                                backgroundColor: "lightblue",
+                                padding: "10px",
+                                borderRadius: "10px",
+                                width: "90%"
+                            }}>
+                                <p>Completion: {msg.completion}</p>
+                            </div>
+                            &nbsp;
+
+                        </div>
+                    ))}
+                </div>
+
+                <form onSubmit={handleSubmit} style={{ display: "flex", alignItems: "center" }}>
+                    <input
+                        style={{
+                            width: "305px",
+                            fontSize: "13px",
+                            padding: "10px",
+                            borderRadius: "5px",
+                            border: "1px solid #ccc",
+                            marginRight: "10px"
+                        }}
+                        type="text"
+                        value={prompt}
+                        placeholder="Enter Message"
+                        onChange={(e) => setPrompt(e.target.value)}
+                    />
+                    <button
+                        style={{
+                            backgroundColor: "white",
+                            color: "white",
+                            padding: "10px 20px",
+                            border: "1px solid #018CBA",
+                            borderRadius: "5px",
+                            cursor: "pointer"
+                        }}
+                        type="button"
+                        onMouseDown={listen}
+                        onMouseUp={stop}
+                    >
+                        🔊
+                    </button>
+                    &nbsp;
+                    <button
+                        style={{
+                            backgroundColor: "#008CBA",
+                            color: "white",
+                            padding: "10px 20px",
+                            border: "none",
+                            borderRadius: "5px",
+                            cursor: "pointer"
+                        }}
+                        type="submit"
+                    >
+                        Send
+                    </button>
+                </form>
+
+
 
             </div>
-          ))}
         </div>
-
-        <form onSubmit={handleSubmit} style={{ display: "flex", alignItems: "center" }}>
-          <input
-            style={{
-              width: "305px",
-              fontSize: "13px",
-              padding: "10px",
-              borderRadius: "5px",
-              border: "1px solid #ccc",
-              marginRight: "10px"
-            }}
-            type="text"
-            value={prompt}
-            placeholder="Enter Message"
-            onChange={(e) => setPrompt(e.target.value)}
-          />
-          <button
-            style={{
-              backgroundColor: "white",
-              color: "white",
-              padding: "10px 20px",
-              border: "1px solid #018CBA",
-              borderRadius: "5px",
-              cursor: "pointer"
-            }}
-            type="button"
-            onMouseDown={listen}
-            onMouseUp={stop}
-          >
-           🔊
-          </button>
-          &nbsp;
-          <button
-            style={{
-              backgroundColor: "#008CBA",
-              color: "white",
-              padding: "10px 20px",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer"
-            }}
-            type="submit"
-          >
-            Send
-          </button>
-        </form>
+    );
 
 
 
-      </div>
-    </div>
-  );
+
+
 };
 
 export default Chat;
