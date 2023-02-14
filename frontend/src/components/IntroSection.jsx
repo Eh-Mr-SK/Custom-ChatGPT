f9067af4d564cd133a8955b94844924c288d551d
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-02-15 00:47:25 +0600

    name changed

--- frontend/src/components/IntroSection.jsx
+++ frontend/src/components/IntroSection.jsx
@@ -1,19 +1,10 @@
 import React from "react";
-// import BotResponse from "./BotResponse";
-
 const IntroSection = () => {
   return (
     <div id="introsection">
       <h1>
         Introducing Custom ChatGPT
-        {/* <BotResponse response=" " /> */}
       </h1>
-      {/* <h2>
-        A cutting-edge AI-powered app that provides instant answers to any
-        question you may have. With Talkbot, you'll never be left searching for
-        answers again. Whether you need information for school or work, or just
-        want to know the latest news, Talkbot has you covered.
-      </h2> */}
       <p>
         Features:
         <ul>
@@ -24,11 +15,7 @@ const IntroSection = () => {
           <li>Available 24/7</li>
         </ul>
       </p>
-      {/* <p>
-        Say goodbye to endless searching and typing, and say hello to TalkBot,
-        your personal AI assistant. Try it now and see for yourself how TalkBot
-        can make your life easier.
-      </p> */}
+   
     </div>
   );
 };
